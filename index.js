341d33bbb20b54c39980acd2d19396f09447b273
cgheorg21 <xampostaunas@gmail.com>
2026-03-30 22:29:24 +0300

    update index DB

--- index.js
+++ index.js
@@ -18,10 +18,7 @@ const axiosInstance = axios.create({
 });
 
 // ================= DB =================
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 
 mongoose.connection.on("error", (err) => {
   console.log("❌ DB ERROR:", err);
